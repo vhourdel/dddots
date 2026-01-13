b275d1c8b7c091c3e3004f85373ea6d83f7e5897
Vincent Hourdel <vincent.hourdel@datadoghq.com>
2026-01-13 10:16:00 +0100

    Remove eza alias

--- .config/fish/functions/openprweb.fish
+++
@@ -1,4 +0,0 @@
-function openprweb --description "Open PR creation UI for current branch on web-ui"
-    set -l BRANCH (git rev-parse --abbrev-ref HEAD)
-    open "https://github.com/DataDog/web-ui/compare/preprod...$BRANCH"
-end
